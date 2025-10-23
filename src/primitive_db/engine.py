49c894603e316010eb527d4cd2b9e3e04d45ff68
Alexander Korotkevich <65502660+Alexander-Korotkevich@users.noreply.github.com>
2025-10-23 19:37:54 +0300

    add prompt lib

---
+++ src/primitive_db/engine.py
@@ -0,0 +1,7 @@
+def welcome():
+    print("Первая попытка запустить проект!\n\n")
+    print("***")
+    print("<command> exit - выйти из программы")
+    print("<command> help - справочная информация")
+    print("Введите команду: help")
+    pass
