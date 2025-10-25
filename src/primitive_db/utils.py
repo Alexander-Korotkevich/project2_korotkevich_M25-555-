9b90a40e5630850cced91a262bbf0367529b42c5
Alexander Korotkevich <65502660+Alexander-Korotkevich@users.noreply.github.com>
2025-10-25 20:39:45 +0300

    add utils functions

---
+++ src/primitive_db/utils.py
@@ -0,0 +1,22 @@
+import json
+
+
+def load_metadata(filepath: str):
+    try:
+        with open(filepath, "r", encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
+    except Exception as e:
+        print(f"Неожиданная ошибка: {e}")
+        return None
+
+
+def save_metadata(filepath: str, data) -> bool:
+    try:
+        with open(filepath, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False)
+            return True
+    except Exception as e:
+        print(f"Ошибка сохранения: {e}")
+        return False
