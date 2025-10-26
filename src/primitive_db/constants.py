a34f1e18c59ce4b0fb9ce26fe2d06c3f61d083f1
Alexander Korotkevich <65502660+Alexander-Korotkevich@users.noreply.github.com>
2025-10-26 16:55:47 +0300

    add simple commands exit and help

--- src/primitive_db/constants.py
+++ src/primitive_db/constants.py
@@ -4,3 +4,9 @@ ID_COLUMN = {
     "name": "ID",
     "type": "int",
 }
+
+CMD_CREATE_TABLE = "create_table"
+CMD_DROP_TABLE = "drop_table"
+CMD_LIST_TABLES = "list_tables"
+CMD_EXIT = "exit"
+CMD_HELP = "help"
--- src/primitive_db/engine.py
+++ src/primitive_db/engine.py
@@ -1,7 +1,48 @@
-def welcome():
-    print("Первая попытка запустить проект!\n\n")
-    print("***")
-    print("<command> exit - выйти из программы")
-    print("<command> help - справочная информация")
-    print("Введите команду: help")
-    pass
+import shlex
+from src.primitive_db.utils import load_metadata, save_metadata
+import prompt
+import src.primitive_db.constants as const
+
+
+def print_help():
+    """Prints the help message for the current mode."""
+
+    print("\n***Процесс работы с таблицей***")
+    print("Функции:")
+    print("<command> create_table <имя_таблицы> <столбец1:тип> .. - создать таблицу")
+    print("<command> list_tables - показать список всех таблиц")
+    print("<command> drop_table <имя_таблицы> - удалить таблицу")
+
+    print("\nОбщие команды:")
+    print("<command> exit - выход из программы")
+    print("<command> help - справочная информация\n")
+
+
+def exit() -> bool:
+    """Выход из программы"""
+
+    print("Программа завершена")
+    return False
+
+
+def run():
+    is_active = True
+
+    while is_active:
+        metadata = load_metadata("./db_meta.json")
+        user_input = prompt.string("Введите команду: ")
+        args = shlex.split(user_input)
+
+        match (args[0]):
+            case const.CMD_CREATE_TABLE:
+                pass
+            case const.CMD_LIST_TABLES:
+                pass
+            case const.CMD_DROP_TABLE:
+                pass
+            case const.CMD_EXIT:
+                is_active = exit()
+                continue
+            case const.CMD_HELP:
+                print_help()
+                continue
